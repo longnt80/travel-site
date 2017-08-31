93e8809c84cf2241c75325ac67d56534a6c6d21c
Long Nguyen <longnt80@gmail.com>
2017-08-31 13:47:00 +0700

    Completed reveal on scroll

---
+++ app/assets/scripts/modules/RevealOnScroll.js
@@ -0,0 +1,32 @@
+import $ from 'jquery';
+import waypoints from '../../../../node_modules/waypoints/lib/noframework.waypoints.js';
+
+class RevealOnScroll {
+  constructor(els, offset) {
+    this.itemsToReveal = els;
+    this.offsetPercentage = offset;
+    this.hideInitially();
+    this.createWaypoints();
+  }
+
+  hideInitially() {
+    console.log(this.itemsToReveal);
+    this.itemsToReveal.addClass("reveal-item");
+  }
+
+  createWaypoints() {
+    var that = this;
+    this.itemsToReveal.each(function() {
+      var currentItem = this;
+      new Waypoint({
+        element: currentItem,
+        handler: function() {
+          $(currentItem).addClass("reveal-item--is-visible");
+        },
+        offset: that.offsetPercentage
+      });
+    });
+  }
+}
+
+export default RevealOnScroll;
